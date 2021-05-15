5552d1cfc36b28066b7e8742ecddcc6e5d2a63c8
David S. García (Azæl) <azael@dragonspark.tech>
2021-05-14 23:59:15 -0500

    Feat: Create core class and solution

---
+++ PayUKit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PayU.Services;
+
+namespace PayU
+{
+    public class PayUKit
+    {
+        public PaymentsService Payments;
+        
+        public PayUKit(string apiLogin, string apiKey, bool testMode, string language)
+        {
+            Payments = new PaymentsService(apiLogin, apiKey, language, testMode);
+        }
+    }
+}
