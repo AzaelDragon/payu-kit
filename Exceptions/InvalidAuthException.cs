64d81d67798e63c3132e2b137de1ef4f45403fbc
David S. García (Azæl) <azael@dragonspark.tech>
2021-05-14 23:57:55 -0500

    Feat: Create base exceptions for basic http negotiation

---
+++ Exceptions/InvalidAuthException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PayU.Exceptions
+{
+    public class InvalidAuthException : Exception
+    {
+        public InvalidAuthException()
+        {
+        }
+
+        public InvalidAuthException(string message) : base(message)
+        {
+        }
+
+        public InvalidAuthException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Exceptions/UnexpectedTransactionException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PayU.Exceptions
+{
+    public class UnexpectedTransactionException : Exception
+    {
+        public UnexpectedTransactionException()
+        {
+        }
+
+        public UnexpectedTransactionException(string message) : base(message)
+        {
+        }
+
+        public UnexpectedTransactionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
